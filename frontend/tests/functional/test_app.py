1f87dbc1cdf474c6a70864ec880bf2c20d459b3a
root <takekazusa@gmail.com>
2018-10-01 18:27:40 +0000

    add tests with pytest

---
+++ frontend/tests/functional/test_app.py
@@ -0,0 +1,14 @@
+# -*- encoding: UTF-8 -*-
+import pytest
+from app import app
+
+def test_main_route(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = test_client.get('/')
+    assert response.status_code == 200
+
+
