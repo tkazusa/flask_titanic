8c351aca65b3386069b95f2f40ad608d2503f1f9
root <takekazusa@gmail.com>
2018-10-04 10:27:45 +0000

    develop frontend

--- frontend/app/routes.py
+++ frontend/app/routes.py
@@ -1,9 +1,24 @@
 # -*- encoding: UTF-8 -*-
-from flask import render_template
+import requests
+import json 
+
+from flask import Flask, request, render_template
 from app import app
 from app.forms import InputForm
 
-@app.route('/')
+
+@app.route('/', methods=["GET"])
 def input():
     form = InputForm()
     return render_template('input.html', title='Input', form=form)
+
+
+@app.route('/', methods=["POST"])
+def result():
+    # response = requests.post(app.config["URL"], data=json.dumps(request.form), headers=app.config["headers"])
+    response = 1
+    if response == 1:
+        status = "ざんねーん、死んじゃうよ！"
+    else:
+        status = "良かったね！生き残るってさ！"
+    return render_template('result.html', status=status)
--- frontend/frontend.py
+++ frontend/frontend.py
@@ -2,7 +2,5 @@
 from app import app
 
 
-app = app
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
